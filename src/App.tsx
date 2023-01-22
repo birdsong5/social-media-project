d2f60311a1a31a97bdd2641a5585c9047c78dea3
birdsong5 <108277183+birdsong5@users.noreply.github.com>
2023-01-22 18:21:10 +0100

    imported react router dom
    and added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
