d9c17f92226a3ac2210642fc202d960197f1e3f6
birdsong5 <108277183+birdsong5@users.noreply.github.com>
2023-03-04 16:31:10 +0100

    updated navbar with firebase

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,10 +1,20 @@
 import { Link } from "react-router-dom";
+import { auth } from "../config/firebase";
 
 export const Navbar = () => {
   return (
     <div>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/Login"}>Log In</Link>
+      <div>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/Login"}>Log In</Link>
+      </div>
+      <p>{auth.currentUser?.displayName}</p>
+      <img
+        src={auth.currentUser?.photoURL || ""}
+        width="100"
+        height="100"
+        alt=""
+      />
     </div>
   );
 };
