789e0661ca72a22860fb09cf9f85b7dd364bd3f5
birdsong5 <108277183+birdsong5@users.noreply.github.com>
2023-04-14 18:45:25 +0200

    update on Post component

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,4 +1,31 @@
+import { getDocs, collection } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { db } from "../config/firebase";
+
+interface Post {
+  id: string;
+  userId: string;
+  title: string;
+  description: string;
+  username: string;
+}
+
 export const Main = () => {
+  const [postList, setPostList] = useState<Post[] | null>(null);
+
+  const postsRef = collection(db, "posts");
+
+  const getPosts = async () => {
+    const data = await getDocs(postsRef);
+    setPostList(
+      data.docs.map((doc) => ({ ...doc.data(), id: doc.id })) as Post[]
+    );
+  };
+
+  useEffect(() => {
+    getPosts();
+  }, []);
+
   return (
     <div>
       <h1>Home page</h1>
